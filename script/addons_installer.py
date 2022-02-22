96d86ee78994eb0e714533c3ddebf78733e5001f
Marcello <marcello.goccia@iit.it>
2022-02-22 07:12:47 -0800

    Added addons installer, created a python script which automatically installs and enables the addons

---
+++ script/addons_installer.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2006-2022 Istituto Italiano di Tecnologia (IIT)
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms of the
+# BSD-3-Clause license. See the accompanying LICENSE file for details.
+
+
+import bpy
+from  distutils import dir_util
+import os
+
+addon_path = bpy.utils.user_resource('SCRIPTS', path="addons", create=True)
+addon_urdfToBlender = os.path.join(addon_path, 'urdfToBlender')
+addon_blenderRCBPanel = os.path.join(addon_path, 'blenderRCBPanel')
+
+origin_path_urdfToBlender = '/home/marcello/teamcode/blender-robotics-utils/script/urdfToBlender/'
+origin_path_blenderRCBPanel = '/home/marcello/teamcode/blender-robotics-utils/script/blenderRCBPanel/'
+
+dir_util.copy_tree(origin_path_urdfToBlender, addon_urdfToBlender) 
+dir_util.copy_tree(origin_path_blenderRCBPanel, addon_blenderRCBPanel) 
+
+bpy.ops.preferences.addon_enable(module='urdfToBlender')
+bpy.ops.preferences.addon_enable(module='blenderRCBPanel')
+bpy.ops.wm.save_userpref()
